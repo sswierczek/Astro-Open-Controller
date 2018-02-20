a211829258dd94a22a095c3c31415b42b20bd1b7
Sebastian Swierczek <sebastian.swierczek@gmail.com>
2018-02-20 21:06:24 +0100

    Move library files to folders. Add DS18B20 sensor support

---
+++ Automatic heater controller/Automatic heater controller/src/ds18b20/onewire.c
@@ -0,0 +1,122 @@
+/* onewire.c - a part of avr-ds18b20 library
+ *
+ * Copyright (C) 2016 Jacek Wieczorek
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.	See the LICENSE file for details.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <inttypes.h>
+
+#include "onewire.h"
+
+uint8_t onewireInit( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask )
+{
+	//Init one wire bus (it's basically reset pulse)
+
+	uint8_t response = 0;
+	uint8_t sreg = SREG; //Store status register
+
+	cli( ); //Disable interrupts
+
+	*port |= mask; //Write 1 to output
+	*direction |= mask; //Set port to output
+	*port &= ~mask; //Write 0 to output
+
+	_delay_us( 600 );
+
+	*direction &= ~mask; //Set port to input
+
+	_delay_us( 100 );
+
+	response = *portin & mask; //Read input
+
+	_delay_us( 200 );
+
+	*port |= mask; //Write 1 to output
+	*direction |= mask; //Set port to output
+
+	_delay_us( 600 );
+
+	SREG = sreg; //Restore status register
+
+	return response != 0 ? ONEWIRE_ERROR_COMM : ONEWIRE_ERROR_OK;
+}
+
+inline uint8_t onewireWriteBit( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask, uint8_t bit )
+{
+	uint8_t sreg = SREG;
+
+	cli( );
+
+	*port |= mask; //Write 1 to output
+	*direction |= mask;
+	*port &= ~mask; //Write 0 to output
+
+	if ( bit != 0 ) _delay_us( 8 );
+	else _delay_us( 80 );
+
+	*port |= mask;
+
+	if ( bit != 0 ) _delay_us( 80 );
+	else _delay_us( 2 );
+
+	SREG = sreg;
+
+	return bit != 0;
+}
+
+void onewireWrite( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask, uint8_t data )
+{
+	//Write byte to one wire bus
+
+	uint8_t sreg = SREG; //Store status register
+	uint8_t i = 0;
+
+	cli( );
+
+	for ( i = 1; i != 0; i <<= 1 ) //Write byte in 8 single bit writes
+		onewireWriteBit( port, direction, portin, mask, data & i );
+
+	SREG = sreg;
+}
+
+inline uint8_t onewireReadBit( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask )
+{
+	uint8_t bit = 0;
+	uint8_t sreg = SREG;
+
+	cli( );
+	*port |= mask; //Write 1 to output
+	*direction |= mask;
+	*port &= ~mask; //Write 0 to output
+	_delay_us( 2 );
+	*direction &= ~mask; //Set port to input
+	_delay_us( 5 );
+	bit = ( ( *portin & mask ) != 0 ); //Read input
+	_delay_us( 60 );
+	SREG = sreg;
+
+	return bit;
+}
+
+uint8_t onewireRead( volatile uint8_t *port, volatile uint8_t *direction, volatile uint8_t *portin, uint8_t mask )
+{
+	//Read byte from one wire data bus
+
+	uint8_t sreg = SREG; //Store status register
+	uint8_t data = 0;
+	uint8_t i = 0;
+
+	cli( ); //Disable interrupts
+
+	for ( i = 1; i != 0; i <<= 1 ) //Read byte in 8 single bit reads
+		data |= onewireReadBit( port, direction, portin, mask ) * i;
+
+	SREG = sreg;
+
+	return data;
+}
--- Automatic heater controller/Automatic heater controller/src/main.c
+++ Automatic heater controller/Automatic heater controller/src/main.c
@@ -4,8 +4,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include "HD44780.h"
-#include "dht.h"
+#include "hd44780/HD44780.h"
+#include "dht/dht.h"
+#include "ds18b20/ds18b20.h"
 
 void LCD_WriteDataArray(const char *buffer)
 {
@@ -19,7 +20,6 @@ void LCD_WriteDataArray(const char *buffer)
 int main (void)
 {
 	
-	board_init();
 	LCD_Initalize();
 	
 	char lcd_cache[100];
@@ -31,40 +31,54 @@ int main (void)
 	float temperature = 0;
 	float humidity = 0;
 	#endif
+	
+	int sensorTemperature;
 
 	for (;;) {
 		
-		if(dht_gettemperaturehumidity(&temperature, &humidity) != -1) {
-
-			LCD_Clear();
-			LCD_GoTo(0,0);
-
-			#if DHT_FLOAT == 0
-			itoa(temperature, lcd_cache, 10);
-			#elif DHT_FLOAT == 1
-			dtostrf(temperature, 3, 1, lcd_cache);
-			#endif
-			
-			LCD_WriteText("Temp: ");
-			LCD_WriteDataArray(lcd_cache);
-			LCD_WriteText(" C");
+		// Get temp and hum from DHT sensor
+		if(dht_gettemperaturehumidity(&temperature, &humidity) == -1) {
 			LCD_GoTo(0,1);
-			
-			#if DHT_FLOAT == 0
-			itoa(humidity, lcd_cache, 10);
-			#elif DHT_FLOAT == 1
-			dtostrf(humidity, 3, 1, lcd_cache);
-			#endif
-			
-			LCD_WriteText("Hum : ");
-			LCD_WriteDataArray(lcd_cache);
-			LCD_WriteText(" %RH");
-			LCD_GoTo(1,0);
-
-			} else {
-			LCD_GoTo(0,1);
-			LCD_WriteText("Error");
+			LCD_WriteText("Error reading DHT state");
 		}
+		
+		// Get temp from DS18B20 sensor
+		ds18b20convert( &PORTB, &DDRB, &PINB, ( 1 << 0 ), NULL );
+
+		//Delay (sensor needs time to perform conversion)
+		_delay_ms( 1000 );
+
+		//Read temperature (without ROM matching)
+		ds18b20read( &PORTB, &DDRB, &PINB, ( 1 << 0 ), NULL, &sensorTemperature );
+		
+		LCD_Clear();
+		LCD_GoTo(0,0);
+		
+		#if DHT_FLOAT == 0
+		itoa(temperature, lcd_cache, 10);
+		#elif DHT_FLOAT == 1
+		dtostrf(temperature, 3, 1, lcd_cache);
+		#endif
+		
+		LCD_WriteText("T:");
+		LCD_WriteDataArray(lcd_cache);
+
+		#if DHT_FLOAT == 0
+		itoa(humidity, lcd_cache, 10);
+		#elif DHT_FLOAT == 1
+		dtostrf(humidity, 3, 1, lcd_cache);
+		#endif
+		
+		LCD_WriteText(", H:");
+		LCD_WriteDataArray(lcd_cache);
+		
+		float sensorRealTemperature = (float) sensorTemperature / DS18B20_MUL;
+		dtostrf(sensorRealTemperature, 3, 1, lcd_cache);
+
+		LCD_GoTo(0,1);
+		LCD_WriteText("T2:");
+		LCD_WriteDataArray(lcd_cache);
+		
 		_delay_ms(1500);
 	}
 
